5e8627e9daf710709cd38134a1e034d27166d925
Ken Barlow <kenbrlow@gmail.com>
2020-08-27 19:58:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
 def square_array(array)
+ new_array = []
  array.each do |number|
    puts "Before #{number}"
    squared = number**2
+   new_array << squared
    puts "After #{squared}"
+   puts "Final Array #{new_array}"
  end
 end
